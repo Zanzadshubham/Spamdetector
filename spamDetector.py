a6d8b56f8b37331aa2a9321da47d65b3412c8892
Zanzadshubham <95909557+Zanzadshubham@users.noreply.github.com>
2021-12-10 14:53:34 +0530

    Add files via upload

---
+++ spamDetector.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+from win32com.client import Dispatch
+from PIL import Image
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd= r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+def speak(text):
+	speak=Dispatch(("SAPI.SpVoice"))
+	speak.Speak(text)
+
+
+model = pickle.load(open('spam.pkl','rb'))
+cv=pickle.load(open('vectorizer.pkl','rb'))
+
+
+def main():
+	st.title("Email & Image Spam Classification Application")
+	st.write("Build with Streamlit & Python")
+	activites=["Classification","About"]
+	choices=st.sidebar.selectbox("Select Activities",activites)
+	if choices=="Classification":
+		st.subheader("Email Classification")
+		msg=st.text_input("Enter a text")
+		if st.button("Process"):
+			print(msg)
+			print(type(msg))
+			data=[msg]
+			print(data)
+			vec=cv.transform(data).toarray()
+			result=model.predict(vec)
+			if result[0]==0:
+				st.success("This is Not A Spam Email")
+				speak("This is Not A Spam Email")
+			else:
+				st.error("This is A Spam Email")
+				speak("This is A Spam Email")
+
+# Function to Read and Manupilate Images
+def load_image(img):
+    im = Image.open(img)
+    image = np.array(im)
+    return image
+
+# Uploading the File to the Page
+st.subheader("Image Classification")
+uploadFile = st.file_uploader(label="Upload image", type=['jpg', 'png'])
+
+# Checking the Format of the page
+if uploadFile is not None:
+    # Perform your Manupilations (In my Case applying Filters)
+    img = load_image(uploadFile)
+    st.image(img)
+    st.write("Image Uploaded Successfully")
+else:
+    st.write("Make sure you image is in JPG/PNG Format.")
+
+if st.button("Check"):
+			im= img
+			text= pytesseract.image_to_string(im, lang= 'en')
+			print(msg)
+			print(type(msg))
+			data=[text]
+			print(text)
+			vec=cv.transform(text).toarray()
+			result=model.predict(vec)
+			if result[0]==0:
+				st.success("This is Not A Spam Email")
+				speak("This is Not A Spam Email")
+			else:
+				st.error("This is A Spam Email")
+				speak("This is A Spam Email")
+main()
